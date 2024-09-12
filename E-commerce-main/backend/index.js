a357daa82b12de4c5b8140cfb7a0795bd9528f79
Asma_Frouja <114805679+asmafrouja@users.noreply.github.com>
2024-09-12 21:16:10 +0100

    change port to 3001 and for backuend 8001 in .env

--- E-commerce-main/backend/index.js
+++ E-commerce-main/backend/index.js
@@ -16,7 +16,7 @@ app.use(cookieParser());
 
 app.use("/api", router);
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8001;
 
 connectDB().then(() => {
     app.listen(PORT, () => {
